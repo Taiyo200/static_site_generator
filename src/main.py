c10088552781a22f5b162212a983dce426e60f7b
Taiyo200 <petrusvlad01@gmail.com>
2025-05-31 19:31:04 +0300

    Deplopying site to GitHub pages with updated code for it

--- src/main.py
+++ src/main.py
@@ -1,15 +1,21 @@
+import sys
 from copy_static import copy_static
 from generator import generate_page, generate_pages_recursive
 
 def main():
+    base_path = "/"
+    if len(sys.argv) > 1:
+        base_path = sys.argv[1]
+        if not base_path.endswith("/"):
+            base_path += "/"
+
     static_path = "static"
-    public_path = "public"
+    output_path = "docs" 
     content_path = "content"
     template_path = "template.html"
 
-    copy_static(static_path, public_path)
-
-    generate_pages_recursive(content_path, template_path, public_path)
+    copy_static(static_path, output_path)
+    generate_pages_recursive(content_path, template_path, output_path, base_path)
 
 
 
